33fb0fec571b25ebdc27835990dc97d40968b307
ducktrshessami <jwee761@gmail.com>
2023-01-17 20:08:45 -0800

    update wrangler-redirect worker

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,19 +1,27 @@
 export interface Env {
-    DESTINATION: string;
+    // Example KV binding
+    KV_NAMESPACE: KVNamespace;
 }
 
 export default {
     async fetch(
         request: Request,
         env: Env,
-        ctx: ExecutionContext
+        _: ExecutionContext
     ): Promise<Response> {
-        const source = new URL(request.url);
-        const destination = new URL(env.DESTINATION);
-        destination.pathname += source.pathname;
-        destination.search = source.search;
-        destination.hash = source.hash;
-        const redirect = new Request(destination.toString(), request);
-        return fetch(redirect);
+        // Example KV key
+        const KV_DESTINATION = await env.KV_NAMESPACE.get("DESTINATION", { type: "text" });
+        if (KV_DESTINATION) {
+            const source = new URL(request.url);
+            const destination = new URL(KV_DESTINATION);
+            destination.pathname += source.pathname;
+            destination.search = source.search;
+            destination.hash = source.hash;
+            const redirect = new Request(destination.toString(), request);
+            return fetch(redirect);
+        }
+        else {
+            return new Response("Redirect destination not set", { status: 404 });
+        }
     },
 };
