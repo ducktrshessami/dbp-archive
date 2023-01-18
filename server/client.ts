75b244e9b6c5d9c094496e2ae71b7d251e3f5483
ducktrshessami <jwee761@gmail.com>
2023-01-17 18:19:24 -0800

    serve client build

---
+++ server/client.ts
@@ -0,0 +1,17 @@
+import { Router, static as serveStatic } from "express";
+import { existsSync } from "fs";
+import { join, resolve } from "path";
+
+const CLIENT_PATH = resolve(__dirname, "..", "client", "build");
+const CLIENT_INDEX = join(CLIENT_PATH, "index.html");
+export const router = Router();
+
+if (existsSync(CLIENT_PATH)) {
+    router
+        .use(serveStatic(CLIENT_PATH))
+        .get("*", (_, res) =>
+            res
+                .status(200)
+                .sendFile(CLIENT_INDEX)
+        );
+}
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import e, { json, urlencoded } from "express";
+import { router as clientRouter } from "./client";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 const app = e()
     .use(urlencoded({ extended: true }))
-    .use(json());
+    .use(json())
+    .use(clientRouter);
 
 app.listen(PORT, () => {
     console.log(`[server] Listening on port ${PORT}`);
