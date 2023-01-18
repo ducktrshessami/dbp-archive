89efd22da456162d7108258e4802ec8fef332222
ducktrshessami <jwee761@gmail.com>
2023-01-18 14:26:47 -0800

    redirect fail gives last set timestamp

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -8,19 +8,27 @@ export default {
         env: Env,
         _: ExecutionContext
     ): Promise<Response> {
-        const KV_DESTINATION = await env.RRP.get("DBP_ARCHIVE", { type: "text" });
-        if (KV_DESTINATION) {
-            const source = new URL(request.url);
-            const destination = new URL(KV_DESTINATION);
-            destination.pathname += source.pathname;
-            destination.search = source.search;
-            destination.hash = source.hash;
-            const redirect = new Request(destination.toString(), request);
-            redirect.headers.set("ngrok-skip-browser-warning", "foobar");
-            return fetch(redirect);
+        const KV_DESTINATION = await env.RRP.getWithMetadata<KvDestinationMetadata>("DBP_ARCHIVE", { type: "text" });
+        if (KV_DESTINATION.value && KV_DESTINATION.metadata) {
+            try {
+                const source = new URL(request.url);
+                const destination = new URL(KV_DESTINATION.value);
+                destination.pathname += source.pathname;
+                destination.search = source.search;
+                destination.hash = source.hash;
+                const redirect = new Request(destination.toString(), request);
+                redirect.headers.set("ngrok-skip-browser-warning", "foobar");
+                return await fetch(redirect);
+            }
+            catch {
+                const timestamp = new Date(KV_DESTINATION.metadata.timestamp);
+                return new Response(`Redirect failed. Last destination set at ${timestamp.toISOString()}`, { status: 502 });
+            }
         }
         else {
             return new Response("Redirect destination not set", { status: 404 });
         }
     },
 };
+
+type KvDestinationMetadata = { timestamp: number };
