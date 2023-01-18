57f34a0361e42ba34b58a9bc18a15f3b001ca41e
ducktrshessami <jwee761@gmail.com>
2023-01-18 14:01:47 -0800

    implement proxy url store

--- server/constants.ts
+++ server/constants.ts
@@ -3,3 +3,5 @@ export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 export const PAGE_LIMIT = process.env.PAGE_LIMIT ? parseInt(process.env.PAGE_LIMIT) : 1000;
 
 export const DB_FORCE = process.env.DB_FORCE ? process.env.DB_FORCE.toLowerCase() !== "false" : false;
+
+export const CF_KEY = process.env.CF_KEY || "DBP_ARCHIVE";
--- server/proxy.ts
+++ server/proxy.ts
@@ -1,5 +1,12 @@
-import { Dispatcher, request } from "undici";
+import {
+    Dispatcher,
+    FormData,
+    request
+} from "undici";
+import { CF_KEY } from "./constants";
 import { RequestError } from "./error";
+
+const WRITE_ENDPOINT = `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT}/storage/kv/namespaces/${process.env.CF_NAMESPACE}/values/${CF_KEY}`;
 
 async function requestWrap(url: string | URL, options?: RequestOptions): Promise<Dispatcher.ResponseData> {
     const res = await request(url, options);
@@ -9,6 +16,17 @@ async function requestWrap(url: string | URL, options?: RequestOptions): Promise
     else {
         throw new RequestError(res.statusCode);
     }
+}
+
+async function storeProxyUrl(url: string): Promise<void> {
+    const data = new FormData();
+    data.set("value", url);
+    data.set("metadata", `{"timestamp":${Date.now()}}`);
+    await requestWrap(WRITE_ENDPOINT, {
+        method: "PUT",
+        headers: { authorization: `Bearer ${process.env.CF_TOKEN}` },
+        body: data
+    });
 }
 
 type RequestOptions = { dispatcher?: Dispatcher } & Omit<Dispatcher.RequestOptions, "origin" | "path" | "method"> & Partial<Pick<Dispatcher.RequestOptions, "method">>;
