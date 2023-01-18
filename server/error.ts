9970d86ab412cf7dc3dd36a4c461befb388556a0
ducktrshessami <jwee761@gmail.com>
2023-01-18 13:48:10 -0800

    request error

---
+++ server/error.ts
@@ -0,0 +1,14 @@
+import { STATUS_CODES } from "http";
+
+class CustomError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+}
+
+export class RequestError extends CustomError {
+    constructor(statusCode: number) {
+        super(`${statusCode} ${STATUS_CODES[statusCode]}`);
+    }
+}
