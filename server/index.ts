113f82a098775abe5e861236ea7a3553c32f5fe4
ducktrshessami <jwee761@gmail.com>
2023-01-17 18:50:38 -0800

    default server port 3001

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import e, { json, urlencoded } from "express";
 import { router as clientRouter } from "./client";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 const app = e()
     .use(urlencoded({ extended: true }))
     .use(json())
