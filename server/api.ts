158f3e543d19f1d15c172e877bc2ff05541836d3
ducktrshessami <jwee761@gmail.com>
2023-01-18 01:06:19 -0800

    channel list route

--- server/api.ts
+++ server/api.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import { existsSync } from "fs";
 import { join, resolve } from "path";
+import { Channel } from "../models";
 
 const ATTACHMENTS_PATH = resolve(__dirname, "..", "attachments");
 export const router = Router();
 
 router
+    .get("/channels", async (_, res) => {
+        const channels = await channelList();
+        res
+            .status(200)
+            .json(channels);
+    })
     .get("/attachment/:filename", (req, res) => {
         const filepath = join(ATTACHMENTS_PATH, req.params.filename);
         if (existsSync(filepath)) {
@@ -19,3 +26,16 @@ router
                 .end();
         }
     });
+
+async function channelList(): Promise<Array<ChannelData>> {
+    const models = await Channel.findAll({ attributes: ["id", "name"] });
+    return models.map(model => ({
+        id: model.id,
+        name: model.name
+    }));
+}
+
+type ChannelData = {
+    id: string,
+    name: string
+};
