f9f516bc7df039dbc246fed84b2536937713021f
ducktrshessami <jwee761@gmail.com>
2023-01-18 00:29:57 -0800

    use api router

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import e, { json, urlencoded } from "express";
+import { router as apiRouter } from "./api";
 import { router as clientRouter } from "./client";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 const app = e()
     .use(urlencoded({ extended: true }))
     .use(json())
+    .use(apiRouter)
     .use(clientRouter);
 
 app.listen(PORT, () => {
