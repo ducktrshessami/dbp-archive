b6b8c46f0c530925453587fd23c996183a0ed5cb
ducktrshessami <jwee761@gmail.com>
2023-01-18 00:29:19 -0800

    attachment route

---
+++ server/api.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { existsSync } from "fs";
+import { join, resolve } from "path";
+
+const ATTACHMENTS_PATH = resolve(__dirname, "..", "attachments");
+export const router = Router();
+
+router
+    .get("/attachment/:filename", (req, res) => {
+        const filepath = join(ATTACHMENTS_PATH, req.params.filename);
+        if (existsSync(filepath)) {
+            res
+                .status(200)
+                .sendFile(filepath);
+        }
+        else {
+            res
+                .status(404)
+                .end();
+        }
+    });
