841fd6d8c064bbd8a3613d10b2747c51c767e800
ducktrshessami <jwee761@gmail.com>
2023-01-19 17:53:43 -0800

    implement get message page route

--- server/api.ts
+++ server/api.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { existsSync } from "fs";
 import { join, resolve } from "path";
-import { Channel, sequelize } from "../models";
+import {
+    Attachment,
+    Break,
+    Channel,
+    Message,
+    sequelize,
+    User
+} from "../models";
 import { PAGE_LIMIT } from "./constants";
 
+const BASE_AVATAR_URL = "https://cdn.discordapp.com/";
 const ATTACHMENTS_PATH = resolve(__dirname, "..", "attachments");
 export const router = Router();
 
@@ -13,6 +21,25 @@ router
         res
             .status(200)
             .json(channels);
+    })
+    .get("/channel/:channelId/:page", async (req, res) => {
+        const pageNumber = parseInt(req.params.page);
+        if (isNaN(pageNumber) || pageNumber.toString() !== req.params.page) {
+            res
+                .status(400)
+                .end();
+            return;
+        }
+        const page = await getChannelPage(req.params.channelId, pageNumber - 1);
+        if (!page) {
+            res
+                .status(404)
+                .end();
+            return;
+        }
+        res
+            .status(200)
+            .json(page);
     })
     .get("/attachment/:filename", (req, res) => {
         const filepath = join(ATTACHMENTS_PATH, req.params.filename);
@@ -43,6 +70,58 @@ async function channelList(): Promise<Array<ChannelData>> {
     }));
 }
 
+function avatarUrl(id: string, discriminator: string, hash?: string): string {
+    return BASE_AVATAR_URL + (hash ? `avatars/${id}/${hash}.png` : `embed/avatars/${parseInt(discriminator) % 5}.png`);
+}
+
+async function getChannelPage(channelId: string, pageIndex: number): Promise<MessagesData | null> {
+    const messages = await Message.findAll({
+        where: { ChannelId: channelId },
+        attributes: ["id", "content", "createdAt"],
+        limit: PAGE_LIMIT,
+        offset: pageIndex * PAGE_LIMIT,
+        include: [
+            {
+                model: User,
+                attributes: ["id", "username", "discriminator", "avatar"]
+            },
+            {
+                model: Attachment,
+                attributes: ["filename"]
+            },
+            {
+                model: Break,
+                attributes: ["MessageId"]
+            }
+        ]
+    });
+    if (!messages.length) {
+        return null;
+    }
+    const [messageData, userCollection] = messages.reduce((data: [Array<MessageData>, Map<string, UserData>], message) => {
+        if (!data[1].has(message.User!.id)) {
+            data[1].set(message.User!.id, {
+                id: message.User!.id,
+                username: message.User!.username,
+                discriminator: message.User!.discriminator,
+                avatarUrl: avatarUrl(message.User!.id, message.User!.discriminator, message.User!.avatar)
+            });
+        }
+        data[0].push({
+            id: message.id,
+            content: message.content,
+            createdAt: message.createdAt.getTime(),
+            break: !!message.Break,
+            attachments: message.Attachments!.map(attachment => attachment.filename)
+        });
+        return data;
+    }, [new Array<MessageData>(), new Map<string, UserData>()]);
+    return {
+        messages: messageData,
+        users: Array.from(userCollection.values())
+    };
+}
+
 interface CountedMessages {
     getDataValue(key: "MessageCount"): number;
 }
@@ -53,4 +132,24 @@ type ChannelData = {
     id: string,
     name: string,
     pages: number
+};
+
+type MessageData = {
+    id: string,
+    content: string,
+    createdAt: number,
+    break: boolean,
+    attachments: Array<string>
+};
+
+type UserData = {
+    id: string,
+    username: string,
+    discriminator: string,
+    avatarUrl: string
+};
+
+type MessagesData = {
+    messages: Array<MessageData>,
+    users: Array<UserData>
 };
