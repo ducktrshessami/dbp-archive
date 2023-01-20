31cdd4d3438061e1e9829bc169aa609f1aed13d9
ducktrshessami <jwee761@gmail.com>
2023-01-19 23:28:06 -0800

    implement api accessor methods

---
+++ client/src/api.ts
@@ -0,0 +1,40 @@
+async function fetchJson(url: string): Promise<any> {
+    const res = await fetch(url);
+    if (res.status < 400) {
+        return await res.json();
+    }
+}
+
+export async function getChannels(): Promise<Array<ChannelData>> {
+    return await fetchJson("/api/channels");
+}
+
+export async function getChannelPage(channelId: string, page: number): Promise<Array<MessagesData>> {
+    return await fetchJson(`/api/channel/${channelId}/${page}`);
+}
+
+type ChannelData = {
+    id: string,
+    name: string,
+    pages: number
+};
+
+type MessageData = {
+    id: string,
+    content: string,
+    createdAt: number,
+    break: boolean,
+    attachments: Array<string>
+};
+
+type UserData = {
+    id: string,
+    username: string,
+    discriminator: string,
+    avatarUrl: string
+};
+
+type MessagesData = {
+    messages: Array<MessageData>,
+    users: Array<UserData>
+};
