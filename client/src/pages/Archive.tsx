67905f60d345fafb913986fa8710393a721e3c45
ducktrshessami <jwee761@gmail.com>
2023-01-20 17:00:07 -0800

    render ChannelList

--- client/src/pages/Archive.tsx
+++ client/src/pages/Archive.tsx
@@ -1,16 +1,18 @@
 import { Component } from "react";
+import ChannelList from "../components/ChannelList";
 import { ChannelData, getChannels } from "../utils/api";
 
 export default class Archive extends Component<{}, ArchiveState> {
     async componentDidMount() {
         const channels = await getChannels();
-        this.state = { channels };
+        this.setState({ channels });
     }
 
     render() {
+        const channelArray = this.state?.channels ? Array.from(this.state.channels.values()) : null;
         return (
             <main>
-
+                <ChannelList channels={channelArray} selected={this.state?.selected} />
             </main>
         );
     }
