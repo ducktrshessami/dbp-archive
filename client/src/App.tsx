7766c9a80ba0aa552050124f4f6e522ea00856fd
ducktrshessami <jwee761@gmail.com>
2023-01-24 16:20:18 -0800

    create Disclaimer and Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import {
     Routes
 } from "react-router-dom";
 import Archive from "./pages/Archive";
+import Disclaimer from "./pages/Disclaimer";
+import Footer from "./components/Footer";
 import SelectRedirect from "./components/SelectRedirect";
 import "./App.css";
 
@@ -13,10 +15,15 @@ export default function App() {
         <div className="App">
             <BrowserRouter>
                 <Routes>
+                    <Route path="/disclaimer" element={<Disclaimer />} />
                     <Route path="/:channelId/:page" element={<Archive />} />
                     <Route path="/:channelId" element={<SelectRedirect />} />
                     <Route path="/" element={<Archive />} />
                     <Route path="/*" element={<Navigate to="/" replace={true} />} />
+                </Routes>
+                <Routes>
+                    <Route path="/disclaimer" element={null} />
+                    <Route path="/*" element={<Footer />} />
                 </Routes>
             </BrowserRouter>
         </div>
