122ad5e9d5ddfd38270537453c918e5c15df1c69
ducktrshessami <jwee761@gmail.com>
2023-01-25 14:36:12 -0800

    partial message sorting

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -3,22 +3,44 @@ import { useNavigate } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import { getChannelPage, MessagesData } from "../utils/api";
 import Message from "./Message";
+import PartialMessage from "./PartialMessage";
 import "./MessageList.css";
+import { MESSAGE_AGEBREAK } from "../utils/constants";
 
 function renderMessageList(channelSelected: boolean, messageData: Nullable<MessagesData>) {
     switch (true) {
         case !!messageData:
-            const messages = messageData!.messages.map(message =>
-                <Message
-                    key={message.id}
-                    id={message.id}
-                    author={messageData!.users.get(message.authorId)!}
-                    content={message.content}
-                    createdAt={new Date(message.createdAt)}
-                    attachments={message.attachments}
-                    break={message.break}
-                />
-            );
+            const messages = messageData!.messages.map((message, i) => {
+                const createdAt = new Date(message.createdAt);
+                if (
+                    message.break ||
+                    i === 0 ||
+                    message.authorId !== messageData!.messages[i - 1].authorId ||
+                    (message.createdAt - messageData!.messages[i - 1].createdAt) >= MESSAGE_AGEBREAK
+                ) {
+                    return (
+                        <Message
+                            key={message.id}
+                            id={message.id}
+                            author={messageData!.users.get(message.authorId)!}
+                            content={message.content}
+                            createdAt={createdAt}
+                            attachments={message.attachments}
+                        />
+                    );
+                }
+                else {
+                    return (
+                        <PartialMessage
+                            key={message.id}
+                            id={message.id}
+                            content={message.content}
+                            createdAt={createdAt}
+                            attachments={message.attachments}
+                        />
+                    );
+                }
+            });
             return (
                 <ul>
                     {messages}
