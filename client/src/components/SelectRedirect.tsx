95e2dde80da3f1570495d8bd566ee57da9f7ad53
ducktrshessami <jwee761@gmail.com>
2023-01-24 17:31:10 -0800

    select redirect replace

--- client/src/components/SelectRedirect.tsx
+++ client/src/components/SelectRedirect.tsx
@@ -3,7 +3,7 @@ import { Navigate, useParams } from "react-router-dom";
 export default function SelectRedirect() {
     const { channelId } = useParams<SelectRedirectParams>();
     return (
-        <Navigate to={`/${channelId}/1`} />
+        <Navigate to={`/${channelId}/1`} replace={true} />
     );
 }
 
