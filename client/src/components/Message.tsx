4811c791b050382082d489534a8bea19f1f9ee6f
ducktrshessami <jwee761@gmail.com>
2023-01-27 14:50:56 -0800

    render non partial message

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,9 +1,15 @@
+import renderAttachments from "../utils/renderAttachments";
 import { MessageProps } from "./MessageProps";
 
 export default function Message(props: MessageProps) {
+    const tag = `${props.author.username}#${props.author.discriminator}`;
     return (
-        <li>
-
+        <li className="message">
+            <img className="avatar" src={props.author.avatarUrl} alt={`${tag}'s avatar`} />
+            <div>
+                <span>{props.content}</span>
+                {renderAttachments(props.attachments)}
+            </div>
         </li>
     );
 }
