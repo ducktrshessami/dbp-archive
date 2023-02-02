3313001ca37a36b4f769c74e026c32f120d75283
ducktrshessami <jwee761@gmail.com>
2023-02-01 18:24:35 -0800

    avatar endpoint

--- server/assets.ts
+++ server/assets.ts
@@ -4,18 +4,33 @@ import { join, resolve } from "path";
 
 const ASSETS_PATH = resolve(__dirname, "..", "assets");
 const ATTACHMENTS_PATH = join(ASSETS_PATH, "attachments");
+const AVATARS_PATH = join(ASSETS_PATH, "avatars");
 export const router = Router();
 
-router.get("/attachment/:filename", (req, res) => {
-    const filepath = join(ATTACHMENTS_PATH, req.params.filename);
-    if (existsSync(filepath)) {
-        res
-            .status(200)
-            .sendFile(filepath);
-    }
-    else {
-        res
-            .status(404)
-            .end();
-    }
-});
+router
+    .get("/attachment/:filename", (req, res) => {
+        const filepath = join(ATTACHMENTS_PATH, req.params.filename);
+        if (existsSync(filepath)) {
+            res
+                .status(200)
+                .sendFile(filepath);
+        }
+        else {
+            res
+                .status(404)
+                .end();
+        }
+    })
+    .get("/avatar/:filename", (req, res) => {
+        const filepath = join(AVATARS_PATH, req.params.filename);
+        if (existsSync(filepath)) {
+            res
+                .status(200)
+                .sendFile(filepath);
+        }
+        else {
+            res
+                .status(404)
+                .end();
+        }
+    });
