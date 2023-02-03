014544e48dd87d48a3a7b1e7735997b5e8e5ff39
ducktrshessami <jwee761@gmail.com>
2023-02-03 14:31:31 -0800

    more explicit message list ready state

--- client/src/components/MessageList.tsx
+++ client/src/components/MessageList.tsx
@@ -8,9 +8,9 @@ import "./MessageList.css";
 import { MESSAGE_AGEBREAK } from "../utils/constants";
 import Notice from "./Notice";
 
-function renderMessageList(channelSelected: boolean, pageSelected: boolean, messageData: Nullable<MessagesData>) {
+function renderMessageList(channelSelected: boolean, ready: boolean, messageData: Nullable<MessagesData>) {
     switch (true) {
-        case channelSelected && pageSelected && !!messageData:
+        case channelSelected && ready && !!messageData:
             const messages = messageData!.messages.map((message, i) => {
                 const createdAt = new Date(message.createdAt);
                 if (
@@ -47,7 +47,7 @@ function renderMessageList(channelSelected: boolean, pageSelected: boolean, mess
                     {messages}
                 </ul>
             );
-        case channelSelected:
+        case channelSelected && !ready:
             return (
                 <div className="centered-box">
                     <BeatLoader color="#8A9A5B" size="1.5rem" />
@@ -64,11 +64,13 @@ export default function MessageList(props: MessageListProps) {
     const navigate = useNavigate();
     const [messageData, setMessagedata] = useState<Nullable<MessagesData>>(null);
     const [fetchedChannel, setFetchedChannel] = useState<Nullable<string>>(null);
+    const [fetchedPage, setFetchedPage] = useState<Nullable<number>>(null);
+    const page = parseInt(props.page!);
+    const ready = fetchedChannel === props.channelId && fetchedPage === page;
 
     useEffect(() => {
         if (props.channelId && props.page && fetchedChannel !== props.channelId) {
             (async function () {
-                const page = parseInt(props.page!);
                 if (isNaN(page) || page.toString() !== props.page) {
                     navigate("/");
                     return;
@@ -76,8 +78,9 @@ export default function MessageList(props: MessageListProps) {
                 try {
                     setMessagedata(null);
                     const data = await getChannelPage(props.channelId!, page);
-                    setMessagedata(data);
                     setFetchedChannel(props.channelId!);
+                    setFetchedPage(page);
+                    setMessagedata(data);
                 }
                 catch (err) {
                     console.error(err);
@@ -89,7 +92,7 @@ export default function MessageList(props: MessageListProps) {
 
     return (
         <div className="message-list">
-            {renderMessageList(!!props.channelId, !!props.page, messageData)}
+            {renderMessageList(!!props.channelId, ready, messageData)}
         </div>
     );
 }
