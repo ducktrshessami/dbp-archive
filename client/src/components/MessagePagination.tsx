964ca69ca354406b617bc816ac4a0eba389adcb3
ducktrshessami <jwee761@gmail.com>
2023-02-05 11:42:08 -0800

    pagination styling

--- client/src/components/MessagePagination.tsx
+++ client/src/components/MessagePagination.tsx
@@ -10,12 +10,19 @@ function renderPagination(navigate: NavigateFunction, props: MessagePaginationPr
                 className="centered-box"
                 forcePage={props.page - 1}
                 pageCount={props.pageCount}
+                pageRangeDisplayed={5}
+                marginPagesDisplayed={1}
                 pageClassName="page"
                 pageLinkClassName="page-link"
                 activeClassName="selected"
                 ariaLabelBuilder={i => `Page ${i}`}
                 previousLabel="ᐸ"
+                previousClassName="page increment"
+                previousLinkClassName="page-link"
                 nextLabel="ᐳ"
+                nextClassName="page increment"
+                nextLinkClassName="page-link"
+                breakClassName="page-break"
                 hrefBuilder={i => `/${props.channelId}/${i}`}
                 onPageChange={({ selected }) => navigate(`/${props.channelId}/${selected + 1}`)}
             />
