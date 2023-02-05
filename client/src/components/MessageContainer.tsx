77f60037aaa81682b2512b115e3c3a961c513efb
ducktrshessami <jwee761@gmail.com>
2023-02-05 09:15:20 -0800

    actual pagination rendering

--- client/src/components/MessageContainer.tsx
+++ client/src/components/MessageContainer.tsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getChannelPage, MessagesData } from "../utils/api";
 import MessageList from "./MessageList";
-import MessagePagination from "./MessagePagination";
+import MessagePagination, { MessagePaginationProps } from "./MessagePagination";
 import "./MessageContainer.css";
 
-function renderMessagePagination(channelSelected: boolean, page?: Nullable<number>, pageCount?: Nullable<number>) {
+function renderMessagePagination(channelSelected: boolean, props: MessagePaginationProps) {
     if (channelSelected) {
         return (
-            <MessagePagination page={page} pageCount={pageCount} />
+            <MessagePagination
+                top={props.top}
+                page={props.page}
+                pageCount={props.pageCount}
+                channelId={props.channelId}
+            />
         );
     }
     else {
@@ -24,9 +29,18 @@ export default function MessageContainer(props: MessageContainerProps) {
     const channelSelected = !!props.channelId;
     const page = parseInt(props.page!);
     const ready = fetchedChannel === props.channelId && fetchedPage === page;
+    const paginationProps: Omit<MessagePaginationProps, "top"> = {
+        page,
+        pageCount: props.pageCount,
+        channelId: props.channelId
+    };
 
     useEffect(() => {
-        if (props.channelId && props.page && fetchedChannel !== props.channelId) {
+        if (
+            props.channelId &&
+            props.page &&
+            (fetchedChannel !== props.channelId || fetchedPage !== page)
+        ) {
             (async function () {
                 if (isNaN(page) || page.toString() !== props.page) {
                     navigate("/");
@@ -49,12 +63,19 @@ export default function MessageContainer(props: MessageContainerProps) {
 
     return (
         <div className="message-container">
-            {renderMessagePagination(channelSelected, page, props.pageCount)}
+            {renderMessagePagination(channelSelected, {
+                ...paginationProps,
+                top: true
+            })}
             <MessageList
                 channelSelected={channelSelected}
                 ready={ready}
                 messageData={messageData}
             />
+            {renderMessagePagination(channelSelected, {
+                ...paginationProps,
+                top: false
+            })}
         </div>
     );
 }
