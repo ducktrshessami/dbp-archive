de00dfe5cc287859eae5837e9136f6ea4484afa5
ducktrshessami <jwee761@gmail.com>
2023-02-06 17:52:24 -0800

    render emojis

--- client/src/utils/renderMessage.tsx
+++ client/src/utils/renderMessage.tsx
@@ -1,9 +1,33 @@
+import reactStringReplace from "react-string-replace";
+import Emoji from "../components/Emoji";
 import attachmentUrl from "./attachmentUrl";
+
+function parseEmojis(content: string) {
+    const idPattern = /^<?(a)?:?\w{2,32}:(?<id>\d{17,19})>?$/;
+    const initialMatch = content.match(idPattern);
+    if (initialMatch) {
+        return (
+            <Emoji id={initialMatch.groups!.id} solo={true} />
+        );
+    }
+    else {
+        return reactStringReplace(content, /(<?(a)?:?\w{2,32}:\d{17,19}>?)/g, match => {
+            const groupedMatch = match.match(idPattern)!;
+            return (
+                <Emoji id={groupedMatch.groups!.id} solo={false} />
+            );
+        });
+    }
+}
+
+function parseContent(content: string) {
+    return parseEmojis(content);
+}
 
 export function renderContent(content: string) {
     if (content.length) {
         return (
-            <span>{content}</span>
+            <span>{parseContent(content)}</span>
         );
     }
     else {
