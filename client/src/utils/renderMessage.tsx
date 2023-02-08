e64a4d206bd0ff4863c2afc738213b8396bbd0bc
ducktrshessami <jwee761@gmail.com>
2023-02-07 20:49:27 -0800

    parseUserMentions util

--- client/src/utils/renderMessage.tsx
+++ client/src/utils/renderMessage.tsx
@@ -1,11 +1,23 @@
 import reactStringReplace from "react-string-replace";
 import Emoji from "../components/Emoji";
+import Mention from "../components/Mention";
+import { UserData } from "./api";
 import attachmentUrl from "./attachmentUrl";
+import userTag from "./userTag";
 
 function parseEmojis(content: string) {
     return reactStringReplace(content, /<?(a)?:?\w{2,32}:(?<id>\d{17,19})>?/, match => (
         <Emoji id={match.groups!.id} solo={match[0].length === content.length} />
     ));
+}
+
+function parseUserMentions(content: string, users: Map<string, UserData> = new Map<string, UserData>()) {
+    return reactStringReplace(content, /<@!?(?<id>\d{17,19})>/, match => {
+        const tag = userTag(users.get(match.groups!.id));
+        return (
+            <Mention>@{tag}</Mention>
+        );
+    });
 }
 
 function parseContent(content: string) {
