56b5456aba635700761f509396246c5679631d93
ducktrshessami <jwee761@gmail.com>
2023-02-09 15:01:15 -0800

    mapMap util

--- client/src/utils/mapUtils.ts
+++ client/src/utils/mapUtils.ts
@@ -2,4 +2,12 @@ export function fromArray<T extends Identifiable>(array: Array<T>): Map<string, 
     return new Map<string, T>(array.map(item => [item.id, item]));
 }
 
+export function mapMap<K, V, T>(map: Map<K, V>, fn: (value: V, key: K, map: Map<K, V>) => T): Array<T> {
+    const iter = map.entries();
+    return Array.from({ length: map.size }, () => {
+        const [key, value] = iter.next().value;
+        return fn(value, key, map);
+    });
+}
+
 type Identifiable = { id: string };
