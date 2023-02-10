674b589aada90da7e940337fb050ca8ed85466a8
ducktrshessami <jwee761@gmail.com>
2023-02-09 17:51:47 -0800

    reduceMap util

--- client/src/utils/mapUtils.ts
+++ client/src/utils/mapUtils.ts
@@ -10,4 +10,19 @@ export function mapMap<K, V, T>(map: Map<K, V>, fn: (value: V, key: K, map: Map<
     });
 }
 
+export function reduceMap<K, V, T>(map: Map<K, V>, fn: (accumulator: T, value: V, key: K, map: Map<K, V>) => T, initialValue?: T): T {
+    if (!map.size) {
+        if (typeof initialValue === "undefined") {
+            throw new TypeError("Reduce of empty map with no initial value");
+        }
+        return initialValue;
+    }
+    const iter = map.entries();
+    let accumulator: T = typeof initialValue === "undefined" ? iter.next().value[1] : initialValue;
+    for (let next = iter.next(); !next.done; next = iter.next()) {
+        accumulator = fn(accumulator, next.value[1], next.value[0], map);
+    }
+    return accumulator;
+}
+
 type Identifiable = { id: string };
