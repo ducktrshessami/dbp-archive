0c973039df23d571058f4a879908e2728d2a2fed
ducktrshessami <jwee761@gmail.com>
2023-02-18 13:56:57 -0800

    remove disclaimer page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,5 @@
-import {
-    BrowserRouter,
-    Route,
-    Routes
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Archive from "./pages/Archive";
-import Disclaimer from "./pages/Disclaimer";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -12,14 +7,8 @@ export default function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/disclaimer" element={<Disclaimer />} />
-                    <Route path="/*" element={<Archive />} />
-                </Routes>
-                <Routes>
-                    <Route path="/disclaimer" element={null} />
-                    <Route path="/*" element={<Footer />} />
-                </Routes>
+                <Archive />
+                <Footer />
             </BrowserRouter>
         </div>
     );
