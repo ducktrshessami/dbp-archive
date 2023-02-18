f000ab61b5dca03f0bc2e7caa167e1aa5353bc89
ducktrshessami <jwee761@gmail.com>
2023-02-18 13:58:20 -0800

    remove disclaimer link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,8 @@
-import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <Link to="/disclaimer">Disclaimer</Link>
             <a href="https://github.com/ducktrshessami/dbp-archive">GitHub</a>
         </footer>
     );
